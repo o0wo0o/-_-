2e195bfcb54a7be89d69c3a1996bd533a03bea8d
lox <ilia28ch@gmail.com>
2025-06-20 18:12:35 +0400

    -_-

---
+++ script.js
@@ -0,0 +1,31 @@
+const { Engine, Render, Runner, World, Bodies } = Matter;
+
+const engine = Engine.create();
+const { world } = engine;
+
+const render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    width: 800,
+    height: 600,
+    wireframes: false,
+    background: '#fafafa',
+  }
+});
+
+Render.run(render);
+Runner.run(Runner.create(), engine);
+
+// Создаём землю
+const ground = Bodies.rectangle(400, 590, 810, 20, { isStatic: true });
+World.add(world, ground);
+
+// Добавляем несколько шаров
+for (let i = 0; i < 5; i++) {
+  const ball = Bodies.circle(400 + i * 30, 100, 20, {
+    restitution: 0.8, // прыгучесть
+    render: { fillStyle: '#f78c6b' }
+  });
+  World.add(world, ball);
+}
