311972d0ace1d91c64585bf385197f74ec737b72
lox <ilia28ch@gmail.com>
2025-06-20 19:06:48 +0400

    0

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+// const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const engine = Engine.create();
-const { world } = engine;
+// const engine = Engine.create();
+// const { world } = engine;
 
-const render = Render.create({
-  element: document.getElementById('scene-container'),
-  engine: engine,
-  options: {
-    width: 800,
-    height: 600,
-    wireframes: false,
-    background: '#fafafa',
-  }
-});
+// const render = Render.create({
+//   element: document.getElementById('scene-container'),
+//   engine: engine,
+//   options: {
+//     width: 800,
+//     height: 600,
+//     wireframes: false,
+//     background: '#fafafa',
+//   }
+// });
 
-Render.run(render);
-Runner.run(Runner.create(), engine);
+// Render.run(render);
+// Runner.run(Runner.create(), engine);
 
-// Создаём землю
-const ground = Bodies.rectangle(400, 590, 810, 20, { isStatic: true });
-World.add(world, ground);
+// // Создаём землю
+// const ground = Bodies.rectangle(400, 590, 810, 20, { isStatic: true });
+// World.add(world, ground);
 
-// Добавляем несколько шаров
-for (let i = 0; i < 5; i++) {
-  const ball = Bodies.circle(400 + i * 30, 100, 20, {
-    restitution: 0.8, // прыгучесть
-    render: { fillStyle: '#f78c6b' }
-  });
-  World.add(world, ball);
-}
-
+// // Добавляем несколько шаров
+// for (let i = 0; i < 5; i++) {
+//   const ball = Bodies.circle(400 + i * 30, 100, 20, {
+//     restitution: 0.8, // прыгучесть
+//     render: { fillStyle: '#f78c6b' }
+//   });
+//   World.add(world, ball);
+// }
 const canvas = document.getElementById('matrix-canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
